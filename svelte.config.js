46692b959c0ace2aec3713a313aae841c4730d04
DUA_Designs <dua99mech.engr@gmail.com>
2024-08-19 21:35:46 +0530

    first commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,24 @@
+
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
-    outDir: 'build', // Output built files to 'build' directory
+    outDir: 'build',
     paths: {
       base: '/Personal-Expense',
+    },
+    prerender: {
+      handleHttpError: ({ status, url }) => {
+        if (status === 404 && url.pathname === '/') {
+          return {
+            status: 301,
+            redirect: '/Personal-Expense',
+          };
+        }
+        return null;
+      },
     },
   },
 };
