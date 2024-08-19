474f2e285c2dc702423a3181f1f30935bc6c8715
DUA_Designs <dua99mech.engr@gmail.com>
2024-08-19 20:15:40 +0530

    first commit

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,12 @@ import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-		  
-	}
+	  kit: {
+    adapter: adapter(),
+    outDir: 'build', // Output built files to 'build' directory
+    base: '/Personal-Expense', // Don't forget to update this too!
+  },
+
 };
 
 export default config;
