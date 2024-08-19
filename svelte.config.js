d59755719befc696bf082c82022ad2e0e104a11f
DUA_Designs <dua99mech.engr@gmail.com>
2024-08-19 21:38:28 +0530

    first commit

--- svelte.config.js
+++ svelte.config.js
@@ -9,17 +9,18 @@ const config = {
     paths: {
       base: '/Personal-Expense',
     },
-    prerender: {
-      handleHttpError: ({ status, url }) => {
-        if (status === 404 && url.pathname === '/') {
-          return {
-            status: 301,
-            redirect: '/Personal-Expense',
-          };
-        }
-        return null;
-      },
-    },
+     prerender: {
+  handleHttpError: ({ status, url }) => {
+    if (status === 404 && url && url.pathname === '/') {
+      return {
+        status: 301,
+        redirect: '/Personal-Expense',
+      };
+    }
+    return null;
+  },
+},
+
   },
 };
 
