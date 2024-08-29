fef11a0cc392be837884208f0351a6ae2185837e
DUA_Designs <dua99mech.engr@gmail.com>
2024-08-29 18:23:26 +0530

    config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,6 +8,6 @@ const config = {
         adapter: adapter(),
         paths:{base:"Personal-Expense"}
     },
-    preprocess: vitePreprocess()
+    preprocess: [vitePreprocess()]
 };
 export default config;
