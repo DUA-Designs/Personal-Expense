d46caa560a431620c9433d4ec23b09494dc562e9
DUA_Designs <dua99mech.engr@gmail.com>
2024-09-03 18:08:55 +0530

    config

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 
-console.log(process.env.BASE_PATH);
+ 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: 'index.html'
+			fallback: '/Personal-Expense/index.html'
 		}),
 		paths: {
 			base:  "/Personal-Expense"
