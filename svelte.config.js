907879bd6a0a7b6fa9f1b9a779fff03305298eaf
DUA_Designs <dua99mech.engr@gmail.com>
2024-09-03 18:12:29 +0530

    config

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,29 @@
-import adapter from '@sveltejs/adapter-static';
 
- 
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '/Personal-Expense/index.html'
-		}),
-		paths: {
-			base:  "/Personal-Expense"
-		}
-	}
+  kit: {
+    adapter: adapter({
+      fallback: '/Personal-Expense/index.html'
+    }),
+    paths: {
+      base: "/Personal-Expense"
+    },
+    prerender: {
+      handleHttpError: ({ statusCode, url }) => {
+        if (statusCode === 404 && url.pathname === '/') {
+          return {
+            statusCode: 301,
+            headers: {
+              'Location': '/Personal-Expense'
+            }
+          };
+        }
+        return null; // default behavior
+      }
+    }
+  }
 };
 
 export default config;
